f23be136c67b3aae48ded071a13a1355d1c02cf5
emhane <74935224+emhane0@users.noreply.github.com>
2022-07-13 00:49:16 +0200

    Delete twitix directory

--- twitix/twitix.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-driver = webdriver.Firefox(executable_path='geckodriver.exe')
-driver.get('https://www.instagram.com/accounts/emailsignup/')
-driver.find_element(By.XPATH, "/html/body/div[4]/div/div/button[2]").click()
