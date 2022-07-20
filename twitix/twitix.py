581c95dc980d8ad98ca6aec81dc7ca738d16307b
emhane <74935224+emhane0@users.noreply.github.com>
2022-07-21 00:06:26 +0200

    twitix with tor foncionnel

--- twitix/twitix.py
+++ twitix/twitix.py
@@ -1,37 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 import os
 import time
-import socket
-import socks
-from urllib.request import urlopen
-from stem import Signal
-from stem.control import Controller
-from selenium.webdriver.firefox.service import Service
 
-os.startfile(r'C:/Users/ethan/Desktop/Tor Browser/Browser/firefox.exe')
-
-controller = Controller.from_port(port=9151)
-
-def connectTor():
-    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9150, True)
-    socket.socket = socks.socksocket
-
-def renewTor():
-    controller.authenticate("abc123")
-    controller.signal(Signal.NEWNYM)
-
-def showIP():
-    print(urlopen('http://icanhazip.com').read())
-
-for i in range(5):
-    renewTor()
-    connectTor()
-    showIP()
-    time.sleep(10)
-
-
-driver = webdriver.Firefox(service= Service(r'geckodriver.exe'))
-
-driver.get('https://www.instagram.com/accounts/emailsignup/')
-driver.find_element(By.XPATH, "/html/body/div[4]/div/div/button[2]").click()
+torexe = os.popen(r'C:\Users\ethan\Desktop\Tor Browser\Browser\TorBrowser\Tortor.exe')
+profile = FirefoxProfile(r'C:\Users\ethan\Desktop\Tor Browser\Browser\TorBrowser\Data\Browser')
+profile.set_preference('network.proxy.type', 1)
+profile.set_preference('network.proxy.socks', '127.0.0.1')
+profile.set_preference('network.proxy.socks_port', 9050)
+profile.set_preference("network.proxy.socks_remote_dns", False)
+profile.update_preferences()
+driver = webdriver.Firefox(firefox_profile= profile, executable_path=r'geckodriver.exe')
+driver.get("http://check.torproject.org")
+time.sleep(10)
